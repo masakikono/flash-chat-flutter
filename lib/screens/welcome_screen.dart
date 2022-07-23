92e76939a28815cdad9e575328a041edef454d5b
Masaki Kono <masaki19931015@gmail.com>
2022-07-23 22:07:34 +0900

    amend: screens directory

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -1,5 +1,3 @@
-import 'package:flash_chat/screens/login_screen.dart';
-import 'package:flash_chat/screens/registration_screen.dart';
 import 'package:flutter/material.dart';
 import 'login_screen.dart';
 import 'registration_screen.dart';
